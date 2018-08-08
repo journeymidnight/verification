a3993b7a92e48d125a5a0e9421f7b0ad5cc8c2a5
yang_tan <yang_tan@haihangyun.com>
2018-08-08 14:23:11 +0800

    oids

--- server.js
+++ server.js
@@ -52,15 +52,16 @@ function getSnmpinfo (host, community, callback) {
       console.log('Get SNMP info error:' + err);
     }
     else {
+      callback(varbinds);
       var snmpStr = '';
-      varbinds.forEach(function(vb) {
-        console.log('Get SNMP info success,HOST:' + host + ',OID:' + oid);      
+      //varbinds.forEach(function(vb) {
+        //console.log('Get SNMP info success,HOST:' + host + ',OID:' + vb.oid);      
         //res.send(vb.oid + ' = ' + vb.value + ' (' + vb.type + ')');
-        snmpStr = snmpStr + vb.oid + ' = ' + vb.value + ' (' + vb.type + ')'
+        //snmpStr = snmpStr + vb.oid + ' = ' + vb.value + ' (' + vb.type + ')'
         //console.log('snmpStr:' + varbinds);
-      });
-      console.log("snmpStr : " + snmpStr);
-      callback(snmpStr);
+      //});
+      //console.log("snmpStr : " + snmpStr);
+      //callback(snmpStr);
     }
     session.close();
   });
