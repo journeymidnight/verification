4709e7a033b05042487f3f98132b0a0580392eb9
yangchong <yangchong@polex.com.cn>
2018-08-16 16:27:41 +0800

    html version

---
+++ main.js
@@ -0,0 +1,38 @@
+var vip = "";
+var view = "";
+var vipInput = document.getElementById("vip");
+var viewNameInput = document.getElementById("view-name");
+var checkBtn = document.getElementById("check");
+
+vipInput.addEventListener("keypress", function (event) {
+    if (event.which === 13) {
+        vip = vipInput.textContent;
+    }
+});
+
+viewNameInput.addEventListener("keypress", function (event) {
+    if (evnet.which === 13) {
+        view = viewNameInput.textContent;
+    }
+});
+
+checkBtn.addEventListener("click", function () {
+
+
+});
+
+function httpGetAsync(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+httpGetAsync("http://127.0.0.1:30000/ceph_status", function(response) {
+    var json = JSON.parse(response);
+    document.getElementById("ceph_status").textContent =  json.osdmap.osdmap.num_osds;
+})
